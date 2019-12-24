0ebfdbdf95d590a926fa3485d5d2028c3a9d3794
Hoonium <hsh8266@gmail.com>
2019-12-24 09:11:25 +0900

    3rd Day_Logic circuit for Machine learning

---
+++ ML(Machine_learning)/logic.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+def AND(x1, x2):
+    x = np.array([x1, x2])        # input
+    w = np.array([0.5, 0.5])      # weights
+    b = 0.75                      # bias
+    y = np.dot(w,x) - b
+    if y <= 0:
+        return 0
+    return 1
+    
+def NAND(x1, x2):
+    return int(not AND(x1,x2))
+    
+def OR(x1, x2):
+    x = np.array([x1, x2])        # input
+    w = np.array([0.5, 0.5])      # weights
+    b = 0.45                      # bias
+    y = np.dot(w,x) - b
+    
+    if y <= 0:
+        return 0
+    return 1
+    
+    
+def XOR(x1, x2):
+    return AND(NAND(x1,x2),OR(x1,x2))

67f8e4523786a565cbe5172231aef60dea9d7695
Hoonium <hsh8266@gmail.com>
2019-12-30 10:04:58 +0900

    4st, 5st Day Feedforward

--- ML(Machine_learning)/logic.py
+++ ML(Machine_learning)/logic.py
@@ -3,8 +3,8 @@ import numpy as np
 def AND(x1, x2):
     x = np.array([x1, x2])        # input
     w = np.array([0.5, 0.5])      # weights
-    b = 0.75                      # bias
-    y = np.dot(w,x) - b
+    b = -0.75                      # bias
+    y = np.dot(w,x) + b
     if y <= 0:
         return 0
     return 1
@@ -15,8 +15,8 @@ def NAND(x1, x2):
 def OR(x1, x2):
     x = np.array([x1, x2])        # input
     w = np.array([0.5, 0.5])      # weights
-    b = 0.45                      # bias
-    y = np.dot(w,x) - b
+    b = -0.45                      # bias
+    y = np.dot(w,x) + b
     
     if y <= 0:
         return 0
